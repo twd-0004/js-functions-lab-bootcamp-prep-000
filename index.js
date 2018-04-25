6e79ae705a9cbde6873c0a2ef5b169426171c05b
Tyler Dreher <drehertyler@gmail.com>
2018-04-25 18:51:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
-function happyHoldaysTo(name) {
+function happyHoldayTo(name) {
   return 'Happy holdays, ${name}!'
 }
 function happyHolidaysTo(holiday, name) {
